b71b7c6dacb653b789a5dc02efaa26f9f26489b5
Daniel Calderón <5720443-dacalderon@users.noreply.gitlab.com>
2021-03-21 02:20:21 -0300

    fixing vao in ex_mouse.py

--- examples/ex_mouse.py
+++ examples/ex_mouse.py
@@ -17,7 +17,8 @@ import sys
 import os.path
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import grafica.transformations as tr
-from grafica.gpu_shape import GPUShape
+import grafica.basic_shapes as bs
+import grafica.easy_shaders as es
 
 __author__ = "Daniel Calderon"
 __license__ = "MIT"
@@ -83,104 +84,6 @@ def scroll_callback(window, x, y):
     print("Mouse scroll:", x, y)
 
 
-def basicShaderProgram():
-
-    # Defining shaders for our pipeline
-    vertex_shader = """
-    #version 130
-    in vec3 position;
-    in vec3 color;
-
-    out vec3 fragColor;
-
-    uniform mat4 transform;
-
-    void main()
-    {
-        fragColor = color;
-        gl_Position = transform * vec4(position, 1.0f);
-    }
-    """
-
-    fragment_shader = """
-    #version 130
-
-    in vec3 fragColor;
-    out vec4 outColor;
-
-    void main()
-    {
-        outColor = vec4(fragColor, 1.0f);
-    }
-    """
-
-    # Assembling the shader program (pipeline) with both shaders
-    shaderProgram = OpenGL.GL.shaders.compileProgram(
-        OpenGL.GL.shaders.compileShader(vertex_shader, GL_VERTEX_SHADER),
-        OpenGL.GL.shaders.compileShader(fragment_shader, GL_FRAGMENT_SHADER))
-
-    return shaderProgram
-
-
-def drawCall(shaderProgram, shape):
-
-    # Binding the proper buffers
-    glBindVertexArray(shape.vao)
-    glBindBuffer(GL_ARRAY_BUFFER, shape.vbo)
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, shape.ebo)
-
-    # Describing how the data is stored in the VBO
-    position = glGetAttribLocation(shaderProgram, "position")
-    glVertexAttribPointer(position, 3, GL_FLOAT, GL_FALSE, 24, ctypes.c_void_p(0))
-    glEnableVertexAttribArray(position)
-    
-    color = glGetAttribLocation(shaderProgram, "color")
-    glVertexAttribPointer(color, 3, GL_FLOAT, GL_FALSE, 24, ctypes.c_void_p(12))
-    glEnableVertexAttribArray(color)
-
-    # This line tells the active shader program to render the active element buffer with the given size
-    glDrawElements(GL_TRIANGLES, shape.size, GL_UNSIGNED_INT, None)
-
-
-def createQuad(r, g, b):
-
-    # Here the new shape will be stored
-    gpuShape = GPUShape()
-
-    # Defining locations and colors for each vertex of the shape    
-    vertexData = np.array([
-    #   positions        colors
-        -0.5, -0.5, 0.0,  r, g, b,
-         0.5, -0.5, 0.0,  r, g, b,
-         0.5,  0.5, 0.0,  r, g, b,
-        -0.5,  0.5, 0.0,  r, g, b
-    # It is important to use 32 bits data
-        ], dtype = np.float32)
-
-    # Defining connections among vertices
-    # We have a triangle every 3 indices specified
-    indices = np.array(
-        [0, 1, 2,
-         2, 3, 0], dtype= np.uint32)
-
-    gpuShape.size = len(indices)
-
-    # VAO, VBO and EBO and  for the shape
-    gpuShape.vao = glGenVertexArrays(1)
-    gpuShape.vbo = glGenBuffers(1)
-    gpuShape.ebo = glGenBuffers(1)
-
-    # Vertex data must be attached to a Vertex Buffer Object (VBO)
-    glBindBuffer(GL_ARRAY_BUFFER, gpuShape.vbo)
-    glBufferData(GL_ARRAY_BUFFER, len(vertexData) * SIZE_IN_BYTES, vertexData, GL_STATIC_DRAW)
-
-    # Connections among vertices are stored in the Elements Buffer Object (EBO)
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, gpuShape.ebo)
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, len(indices) * SIZE_IN_BYTES, indices, GL_STATIC_DRAW)
-
-    return gpuShape
-
-
 if __name__ == "__main__":
 
     # Initialize glfw
@@ -210,21 +113,26 @@ if __name__ == "__main__":
     glfw.set_mouse_button_callback(window, mouse_button_callback)
     glfw.set_scroll_callback(window, scroll_callback)
 
-
-    # Using the simple shader program from basic_shapes and
-    # telling OpenGL to use our shader program
-    shaderProgram = basicShaderProgram()
-    glUseProgram(shaderProgram)
-
+    # Creating our shader program and telling OpenGL to use it
+    pipeline = es.SimpleTransformShaderProgram()
+    glUseProgram(pipeline.shaderProgram)
 
     # Setting up the clear screen color
     glClearColor(0.15, 0.15, 0.15, 1.0)
 
+    # Convenience function to ease initialization
+    def createGPUColorQuad(r,g,b):
+        shapeQuad = bs.createColorQuad(r,g,b)
+        gpuQuad = es.GPUShape().initBuffers()
+        pipeline.setupVAO(gpuQuad)
+        gpuQuad.fillBuffers(shapeQuad.vertices, shapeQuad.indices, GL_STATIC_DRAW)
+        return gpuQuad
+
     # Creating shapes on GPU memory
-    blueQuad = createQuad(0,0,1)
-    redQuad = createQuad(1,0,0)
-    yellowQuad = createQuad(1,1,0)
-    greenQuad = createQuad(0,1,0)
+    blueQuad = createGPUColorQuad(0,0,1)
+    redQuad = createGPUColorQuad(1,0,0)
+    yellowQuad = createGPUColorQuad(1,1,0)
+    greenQuad = createGPUColorQuad(0,1,0)
 
     glPolygonMode(GL_FRONT_AND_BACK, GL_FILL)
 
@@ -249,39 +157,39 @@ if __name__ == "__main__":
                 controller.theta -= 2 * np.pi
         
         # Setting transform and drawing the rotating red quad
-        glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "transform"), 1, GL_TRUE, tr.matmul([
+        glUniformMatrix4fv(glGetUniformLocation(pipeline.shaderProgram, "transform"), 1, GL_TRUE, tr.matmul([
             tr.rotationZ(controller.theta),
             tr.translate(0.5, 0.0, 0.0),
             tr.uniformScale(0.5)
         ]))
-        drawCall(shaderProgram, redQuad)
+        pipeline.drawCall(redQuad)
 
         # Getting the mouse location in opengl coordinates
         mousePosX = 2 * (controller.mousePos[0] - width/2) / width
         mousePosY = 2 * (height/2 - controller.mousePos[1]) / height
  
-        glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "transform"), 1, GL_TRUE, np.matmul(
+        glUniformMatrix4fv(glGetUniformLocation(pipeline.shaderProgram, "transform"), 1, GL_TRUE, np.matmul(
             tr.translate(mousePosX, mousePosY, 0),
             tr.uniformScale(0.3)
         ))
-        drawCall(shaderProgram, greenQuad)
+        pipeline.drawCall(greenQuad)
 
         # This is another way to work with keyboard inputs
         # Here we request the state of a given key
         if (glfw.get_key(window, glfw.KEY_LEFT_CONTROL) == glfw.PRESS):
-            glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "transform"), 1, GL_TRUE, np.matmul(
+            glUniformMatrix4fv(glGetUniformLocation(pipeline.shaderProgram, "transform"), 1, GL_TRUE, np.matmul(
                 tr.translate(-0.6, 0.4, 0.0),
                 tr.uniformScale(0.2)
             ))
-            drawCall(shaderProgram, blueQuad)
+            pipeline.drawCall(blueQuad)
 
         # All "non-pressed" keys are in release state
         if (glfw.get_key(window, glfw.KEY_LEFT_CONTROL) == glfw.RELEASE):
-            glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "transform"), 1, GL_TRUE, np.matmul(
+            glUniformMatrix4fv(glGetUniformLocation(pipeline.shaderProgram, "transform"), 1, GL_TRUE, np.matmul(
                 tr.translate(-0.6, 0.6, 0.0),
                 tr.uniformScale(0.2)
             ))
-            drawCall(shaderProgram, yellowQuad)
+            pipeline.drawCall(yellowQuad)
 
         # Once the render is done, buffers are swapped, showing only the complete scene.
         glfw.swap_buffers(window)
