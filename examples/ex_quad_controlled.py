6fabb8fd1eef029f3379dba8c5be6982c8d8019b
Daniel Calderón <5720443-dacalderon@users.noreply.gitlab.com>
2021-03-21 02:10:07 -0300

    fixing vao in ex_quad_controlled.py

--- examples/ex_quad_controlled.py
+++ examples/ex_quad_controlled.py
@@ -7,15 +7,12 @@ import OpenGL.GL.shaders
 import numpy as np
 import sys, os.path
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from grafica.gpu_shape import GPUShape
 import grafica.transformations as tr
+import grafica.basic_shapes as bs
+import grafica.easy_shaders as es
 
 __author__ = "Daniel Calderon"
 __license__ = "MIT"
-
-# We will use 32 bits data, so an integer has 4 bytes
-# 1 byte = 8 bits
-SIZE_IN_BYTES = 4
 
 
 # A class to store the application control
@@ -59,66 +56,6 @@ def on_key(window, key, scancode, action, mods):
         print('Unknown key')
 
 
-def drawCall(shaderProgram, shape):
-
-    # Binding the proper buffers
-    glBindVertexArray(shape.vao)
-    glBindBuffer(GL_ARRAY_BUFFER, shape.vbo)
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, shape.ebo)
-
-    # Describing how the data is stored in the VBO
-    position = glGetAttribLocation(shaderProgram, "position")
-    glVertexAttribPointer(position, 3, GL_FLOAT, GL_FALSE, 24, ctypes.c_void_p(0))
-    glEnableVertexAttribArray(position)
-    
-    color = glGetAttribLocation(shaderProgram, "color")
-    glVertexAttribPointer(color, 3, GL_FLOAT, GL_FALSE, 24, ctypes.c_void_p(12))
-    glEnableVertexAttribArray(color)
-
-    # This line tells the active shader program to render the active element buffer with the given size
-    glDrawElements(GL_TRIANGLES, shape.size, GL_UNSIGNED_INT, None)
-
-
-def createQuad():
-
-    # Here the new shape will be stored
-    gpuShape = GPUShape()
-
-    # Defining locations and colors for each vertex of the shape
-    
-    vertexData = np.array([
-    #   positions        colors
-        -0.5, -0.5, 0.0,  1.0, 0.0, 0.0,
-         0.5, -0.5, 0.0,  0.0, 1.0, 0.0,
-         0.5,  0.5, 0.0,  0.0, 0.0, 1.0,
-        -0.5,  0.5, 0.0,  1.0, 1.0, 1.0
-    # It is important to use 32 bits data
-        ], dtype = np.float32)
-
-    # Defining connections among vertices
-    # We have a triangle every 3 indices specified
-    indices = np.array(
-        [0, 1, 2,
-         2, 3, 0], dtype= np.uint32)
-
-    gpuShape.size = len(indices)
-
-    # VAO, VBO and EBO and  for the shape
-    gpuShape.vao = glGenVertexArrays(1)
-    gpuShape.vbo = glGenBuffers(1)
-    gpuShape.ebo = glGenBuffers(1)
-
-    # Vertex data must be attached to a Vertex Buffer Object (VBO)
-    glBindBuffer(GL_ARRAY_BUFFER, gpuShape.vbo)
-    glBufferData(GL_ARRAY_BUFFER, len(vertexData) * SIZE_IN_BYTES, vertexData, GL_STATIC_DRAW)
-
-    # Connections among vertices are stored in the Elements Buffer Object (EBO)
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, gpuShape.ebo)
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, len(indices) * SIZE_IN_BYTES, indices, GL_STATIC_DRAW)
-
-    return gpuShape
-
-
 if __name__ == "__main__":
 
     # Initialize glfw
@@ -128,7 +65,7 @@ if __name__ == "__main__":
     width = 600
     height = 600
 
-    window = glfw.create_window(width, height, "Controlling a quad", None, None)
+    window = glfw.create_window(width, height, "Mind Controlled Quad", None, None)
 
     if not window:
         glfw.terminate()
@@ -139,48 +76,18 @@ if __name__ == "__main__":
     # Connecting the callback function 'on_key' to handle keyboard events
     glfw.set_key_callback(window, on_key)
 
-    # Defining shaders for our pipeline
-    vertex_shader = """
-    #version 130
-    in vec3 position;
-    in vec3 color;
-
-    out vec3 fragColor;
-
-    uniform mat4 transform;
-
-    void main()
-    {
-        fragColor = color;
-        gl_Position = transform * vec4(position, 1.0f);
-    }
-    """
-
-    fragment_shader = """
-    #version 130
-
-    in vec3 fragColor;
-    out vec4 outColor;
-
-    void main()
-    {
-        outColor = vec4(fragColor, 1.0f);
-    }
-    """
-
-    # Assembling the shader program (pipeline) with both shaders
-    shaderProgram = OpenGL.GL.shaders.compileProgram(
-        OpenGL.GL.shaders.compileShader(vertex_shader, GL_VERTEX_SHADER),
-        OpenGL.GL.shaders.compileShader(fragment_shader, GL_FRAGMENT_SHADER))
-    
-    # Telling OpenGL to use our shader program
-    glUseProgram(shaderProgram)
+    # Creating our shader program and telling OpenGL to use it
+    pipeline = es.SimpleTransformShaderProgram()
+    glUseProgram(pipeline.shaderProgram)
 
     # Setting up the clear screen color
     glClearColor(0.15, 0.15, 0.15, 1.0)
 
     # Creating shapes on GPU memory
-    gpuQuad = createQuad()
+    shapeQuad = bs.createRainbowQuad()
+    gpuQuad = es.GPUShape().initBuffers()
+    pipeline.setupVAO(gpuQuad)
+    gpuQuad.fillBuffers(shapeQuad.vertices, shapeQuad.indices, GL_STATIC_DRAW)
 
     glPolygonMode(GL_FRONT_AND_BACK, GL_FILL)
 
@@ -203,11 +110,11 @@ if __name__ == "__main__":
             controller.theta += dt
 
         # Drawing the Quad with the given transformation
-        glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "transform"), 1, GL_TRUE, np.matmul(
+        glUniformMatrix4fv(glGetUniformLocation(pipeline.shaderProgram, "transform"), 1, GL_TRUE, np.matmul(
                 tr.translate(controller.x, controller.y, 0.0),
                 tr.rotationZ(controller.theta)
             ))
-        drawCall(shaderProgram, gpuQuad)
+        pipeline.drawCall(gpuQuad)
 
         # Once the render is done, buffers are swapped, showing only the complete scene.
         glfw.swap_buffers(window)
